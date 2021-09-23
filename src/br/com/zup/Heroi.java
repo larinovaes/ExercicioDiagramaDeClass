67aa96329a4ffcfac010155379ff3f922c0ae9c0
Larissa Novais <larinovais54@gmail.com>
2021-09-23 11:56:07 -0300

    Criando classes mãe

---
+++ src/br/com/zup/Heroi.java
@@ -0,0 +1,26 @@
+package br.com.zup;
+
+public class Heroi {
+    private String nome;
+    private double vida;
+
+    public Heroi() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
+}
---
+++ src/br/com/zup/Inimigos.java
@@ -0,0 +1,27 @@
+package br.com.zup;
+
+public class Inimigos {
+    private String nome;
+    private double vida;
+
+    public Inimigos() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
+}
+
